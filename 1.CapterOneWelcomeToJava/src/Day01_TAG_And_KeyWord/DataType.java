8eb1aaf5b95a9fb9e7cc368feb0131c5075c545e
songb <songbaodu@163.com>
2019-09-30 10:09:35 +0800

    HelloWorld练习

--- 1.CapterOneWelcomeToJava/src/Day01_TAG_And_KeyWord/DataType.java
+++ 1.CapterOneWelcomeToJava/src/Day01_TAG_And_KeyWord/DataType.java
@@ -1,5 +1,28 @@
 package Day01_TAG_And_KeyWord;
 
 public class DataType {
+    public static void main(String[] args) {
 
+//        数据类型 变量名称 = 常量;
+//        基本数据类型:
+        int x = 10;//整形
+
+        short y = 1;//短整型
+
+        long z = 99999999;//长整型
+
+        float a = 1.12f;//单精度浮点型
+
+        double b = 3.1415926d;//双精度浮点型
+
+        char c = 'A';//字符型
+        System.out.println(c);
+        byte d = 97;//字节
+        System.out.println(d);
+        boolean e = true;//布尔型
+//        引用数据类型:
+        /*类(class)
+        * 接口(interface)
+        * 数组(array)*/
+    }
 }
